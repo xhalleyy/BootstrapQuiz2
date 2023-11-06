109b862117a5b03ee091ee1c20e1b83dfabf9949
Halley Pham <xhalleyy@gamil.com>
2023-11-06 09:39:03 -0800

    quiz

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+
+let affect = document.getElementById("affect");
+let buttonChange = document.getElementById("buttonChange");
+
+// affectMe.addEventListener("click", function(e){
+//     affectMe.className = "spinner";
+// });
+
+buttonChange.addEventListener("click", function(e){
+    affect.className = "spinner";
+});
